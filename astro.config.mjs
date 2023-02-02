2fe02ccf9342ac45fce724698c4eca9efe5544d6
Sebastian Bartmann <>
2023-02-02 07:27:20 +0100

    change to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), svelte()],
+  adapter: netlify()
 });
