46c979793951065aafb50d96e8386a34c0be1115
bartmann.xyz <7301886+sebastianbartmann@users.noreply.github.com>
2023-03-13 19:37:36 +0100

    add gateway api url incl. key

--- src/services/gcp-services/getAppartmentWeeklyMedianPrices.js
+++ src/services/gcp-services/getAppartmentWeeklyMedianPrices.js
@@ -2,7 +2,7 @@ let data;
 
 export async function getWeeklyMedianPrices() {
 
-    const response = await fetch('x');
+    const response = await fetch('https://wohnpreise-gateway-40p1j3v3.ew.gateway.dev/wohnpreise-weekly-median?key=AIzaSyCptUBI9j-AjUgisWjEL7-plOZENTqCons');
 
     data = response.json();
     return data;
